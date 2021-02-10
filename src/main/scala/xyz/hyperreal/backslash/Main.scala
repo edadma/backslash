12f39bb1e0db97d326b33a57494143c47cd0d974
Edward A Maxedon, Sr <edadma@vinctus.com>
2021-02-10 08:41:08 -0500

    rewrite options parsing

--- src/main/scala/xyz/hyperreal/backslash/Main.scala
+++ src/main/scala/xyz/hyperreal/backslash/Main.scala
@@ -12,32 +12,6 @@ object Main extends App {
       "include" -> ".",
       "rounding" -> "HALF_EVEN"
     )
-  val assigns = new mutable.HashMap[String, Any]
-
-  def json(src: io.Source): Unit =
-    for ((k: String, v) <- DefaultJSONReader
-           .fromString(src mkString)
-           .asInstanceOf[Map[String, Any]])
-      assigns(k) = v
-
-  def run(a: Args): Unit = {
-    val parser = new Parser(Command.standard)
-    val renderer = new Renderer(parser, config)
-    val out =
-      a.out match {
-        case None    => Console.out
-        case Some(f) => new PrintStream(new FileOutputStream(f))
-      }
-
-    util.Using(io.Source.fromFile(a.input))(r =>
-      renderer.render(parser.parse(r), assigns, out))
-  }
-
-  case class Args(strings: Map[String, String],
-                  numbers: Map[String, BigDecimal],
-                  json: Option[String],
-                  input: String,
-                  out: Option[File])
 
   val parser = new scopt.OptionParser[Args]("backslash") {
     head("backslash", "0.4.23")
@@ -51,7 +25,6 @@ object Main extends App {
       .action((x, c) => c.copy(numbers = x))
       .text("numerical variable assignments")
     opt[Option[File]]('o', "out")
-      .optional()
       .valueName("<output file>")
       .action((x, c) => c.copy(out = x))
       .validate(x =>
@@ -78,8 +51,30 @@ object Main extends App {
   }
 
   parser.parse(args, Args(Map(), Map(), None, null, None)) match {
-    case Some(a) => run(args)
-    case None    =>
+    case Some(a) =>
+      val parser = new Parser(Command.standard)
+      val renderer = new Renderer(parser, config)
+      val out =
+        a.out match {
+          case None    => Console.out
+          case Some(f) => new PrintStream(new FileOutputStream(f))
+        }
+      val assigns = new mutable.HashMap[String, Any]
+
+      for ((k: String, v) <- DefaultJSONReader
+             .fromString(src mkString)
+             .asInstanceOf[Map[String, Any]])
+        assigns(k) = v
+
+      util.Using(io.Source.fromFile(a.input))(r =>
+        renderer.render(parser.parse(r), assigns, out))
+    case None =>
   }
+
+  case class Args(strings: Map[String, String],
+                  numbers: Map[String, BigDecimal],
+                  json: Option[String],
+                  input: String,
+                  out: Option[File])
 
 }
